b750ce88c68e1c4a8784f99ad2368c1432349c84
mgrinko <glukym@gmail.com>
2020-07-14 15:12:26 +0300

    Updates

---
+++ precache-manifest.af0346173530d76fea9394c9f71fcd00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167df69c0cf4e5bb166905f181d06cf7",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "ba74609457aa6861f066",
+    "url": "/react-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9e9f67bad663953fe5d",
+    "url": "/react-demo/static/js/2.d39f5f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.d39f5f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba74609457aa6861f066",
+    "url": "/react-demo/static/js/main.42ff8fad.chunk.js"
+  },
+  {
+    "revision": "e91f5e23d011ab615da4",
+    "url": "/react-demo/static/js/runtime-main.5f8d9a6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
